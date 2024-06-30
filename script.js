fe28d6cc77a057daf6c4af0eeb67e675d4ebfcaf
scfulga <146138900+scfulga@users.noreply.github.com>
2024-06-30 19:31:52 +0300

    Added createGrid() function that creates the grid with the chosen number by the user

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let promptInput = prompt('Enter the grid size (Between 1 - 100)');
-console.log(promptInput);
+const container = document.querySelector("#container");;
 
-while (isNaN(promptInput)){
-    alert('Invalid, choose a number');
+function createGrid(){
+    let widthValue = `calc(600px / ${promptInput})`;
+    let heightValue = widthValue;
+
+    for(i = 0; i < (promptInput * promptInput); i++){
+        let div = document.createElement("div"); 
+        div.setAttribute("style", `width: ${widthValue}; height: ${heightValue}; border-color: black; border-width: 1px; border-style: solid; `);
+        container.appendChild(div);
+    }
+
+}
+
+while (isNaN(promptInput) || promptInput < 1 || promptInput > 100 ){
+    alert('Invalid');
     promptInput = prompt('Enter the grid size (Between 1 - 100)');
+    createGrid();
 
-} 
+}
+
+createGrid();
