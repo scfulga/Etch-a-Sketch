f45b61f819867f1c1aa7cdc36bd2009c5988ce5d
scfulga <146138900+scfulga@users.noreply.github.com>
2024-07-01 11:10:24 +0300

    Added a button that changes the grid size (between 1-100)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let promptInput = prompt('Enter the grid size (Between 1 - 100)');
-const container = document.querySelector("#container");;
-
+const container = document.querySelector("#container");
+const gridBtn = document.querySelector("#grid");
+let promptInput = 0;
 function createGrid(){
     let widthValue = `calc(600px / ${promptInput})`;
     let heightValue = widthValue;
@@ -13,11 +13,20 @@ function createGrid(){
 
 }
 
-while (isNaN(promptInput) || promptInput < 1 || promptInput > 100 ){
-    alert('Invalid');
+gridBtn.addEventListener('click', () => {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
+
+    promptInput = Number(promptInput); 
     promptInput = prompt('Enter the grid size (Between 1 - 100)');
-    createGrid();
+    if(promptInput >= 1 && promptInput <= 100){
+        createGrid();
+    }
+    else{
+        promptInput = 0;
+        alert("Invalid");
+    }
+})
 
-}
 
-createGrid();
